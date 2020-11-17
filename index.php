3d12043c878d83d4e30d3b88464c7c0266bafcac
TechMainul <nilakashkhan2@gmail.com>
2020-11-18 04:41:50 +0600

    admin bar added by function

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DevTheme</title>
+    <title><?php bloginfo('name'); ?></title>
 
     <?php wp_head() ; ?>
 </head>
