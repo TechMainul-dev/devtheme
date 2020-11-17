ca30240e1193e0aa6efbf9bee8267a5d9059c432
TechMainul <nilakashkhan2@gmail.com>
2020-11-18 04:50:54 +0600

    load_theme_textdomain added

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
-<?php 
+<?php
+function devtheme_setup() {
+    load_theme_textdomain('devtheme');
+}
+
+
 // Assets
- function dev_stylesheet() {
+ function devtheme_stylesheet() {
     //  wp_enqueue_style( $handle:string, $src:string, $deps:array, $ver:string|boolean|null, $media:string )
-     wp_enqueue_style('dev_main_style', get_stylesheet_uri(  ), '', '1.0.0');
+     wp_enqueue_style('devtheme_main_style', get_stylesheet_uri(  ), '', '1.0.0');
  }
 
 //  add_action( $tag:string, $function_to_add:callable, $priority:integer, $accepted_args:integer )
- add_action('wp_enqueue_scripts', 'dev_stylesheet');
+ add_action('wp_enqueue_scripts', 'devtheme_stylesheet');
