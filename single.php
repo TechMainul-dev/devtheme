1212a7439185bf3db7b0bdec22e9792e02bcd7a8
TechMainul <nilakashkhan2@gmail.com>
2020-11-20 21:46:00 +0600

    single.php modify base index.php

---
+++ single.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+<div class="dev-body">
+    <div class="container">
+        <div class="row">
+            <!-- Post -->
+            <div class="col-md-9">
+                <section class="post-section">
+
+                    <?php  
+                            if (have_posts(  )):
+                                while (have_posts(  )) : the_post(  );
+                        ?>
+
+                    <div class="dev-article">
+                        <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+                        <!-- Feature image -->
+                        <?php 
+                                if(has_post_thumbnail()) {
+                                    the_post_thumbnail("large");
+                                }
+                            ?>
+                        <!-- Date -->
+                        <div class="post-date">
+                            <em>
+                                <?php echo get_the_date(); ?>
+                                | By <?php the_author(); ?></em>
+                        </div>
+                        <p><?php the_content(); ?></p>
+                    </div>
+
+                    <?php
+                            endwhile;                        
+                                else: echo 'no post';
+                            endif;
+                        ?>
+                    <!-- Pagination function & Hook -->
+                    <div class="pagination">
+                        <?php the_posts_pagination(array(
+                                'screen_reader_text' => ' ',
+                                'prev_text' => '<',
+                                'next_text' => '>'
+                            )); ?>
+                    </div>
+
+                </section>
+            </div>
+            <!-- Sidebar -->
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+<!-- Bottom menu -->
+<div class="bottom-menu">
+    <div class="menu">
+        <?php wp_nav_menu(array('theme_location' => 'bottommenu')); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
