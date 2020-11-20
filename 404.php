ccdf4101a9a0346ad10d540cbcab26b16a913436
TechMainul <nilakashkhan2@gmail.com>
2020-11-21 05:37:55 +0600

    404 error page added

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div class="container">
+    <h1 class="text-center">404 Error !</h1>
+
+</div>
+
+<!-- Bottom menu -->
+<div class="bottom-menu">
+    <div class="menu">
+        <?php wp_nav_menu(array('theme_location' => 'bottommenu')); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
