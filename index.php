78c56aca6bcc08de6f24db8c34af588066955d37
TechMainul <nilakashkhan2@gmail.com>
2020-11-20 21:33:00 +0600

    Separating Template header.php | footer.php

--- index.php
+++ index.php
@@ -1,78 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header(); ?>
+<div class="dev-body">
+    <div class="container">
+        <div class="row">
+            <!-- Post -->
+            <div class="col-md-9">
+                <section class="post-section">
 
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php wp_head() ; ?>
-</head>
-
-<body <?php body_class(); ?>>
-    <header class="header-area">
-        <div class="header-content">
-            <div class="logo">
-                <?php the_custom_logo(); ?>
-            </div>
-            <div class="content">
-                <h1><?php bloginfo('name'); ?></h1>
-                <p><?php bloginfo('description'); ?></p>
-                <!-- Top menu  -->
-                <div class="menu">
-                    <?php wp_nav_menu(array('theme_location' => 'topmenu')); ?>
-                </div>
-            </div>
-        </div>
-    </header>
-    <div class="dev-body">
-        <div class="container">
-            <div class="row">
-                <!-- Post -->
-                <div class="col-md-9">
-                    <section class="post-section">
-
-                        <?php  
+                    <?php  
                             if (have_posts(  )):
                                 while (have_posts(  )) : the_post(  );
                         ?>
 
-                        <div class="dev-article">
-                            <h2><?php the_title(); ?></h2>
-                            <!-- Feature image -->
-                            <?php 
+                    <div class="dev-article">
+                        <h2><?php the_title(); ?></h2>
+                        <!-- Feature image -->
+                        <?php 
                                 if(has_post_thumbnail()) {
                                     the_post_thumbnail("large");
                                 }
                             ?>
-                            <!-- Date -->
-                            <div class="post-date">
-                                <em>
-                                    <?php echo get_the_date(); ?>
-                                    | By <?php the_author(); ?></em>
-                            </div>
-                            <p><?php the_content(); ?></p>
+                        <!-- Date -->
+                        <div class="post-date">
+                            <em>
+                                <?php echo get_the_date(); ?>
+                                | By <?php the_author(); ?></em>
                         </div>
+                        <p><?php the_content(); ?></p>
+                    </div>
 
-                        <?php
+                    <?php
                             endwhile;                        
                                 else: echo 'no post';
                             endif;
                         ?>
-                        <!-- Pagination function & Hook -->
-                        <div class="pagination">
-                            <?php the_posts_pagination(array(
+                    <!-- Pagination function & Hook -->
+                    <div class="pagination">
+                        <?php the_posts_pagination(array(
                                 'screen_reader_text' => ' ',
                                 'prev_text' => '<',
                                 'next_text' => '>'
                             )); ?>
-                        </div>
+                    </div>
 
-                    </section>
-                </div>
-                <!-- Sidebar -->
-                <div class="col-md-3">
-                    <?php dynamic_sidebar('right-sidebar'); ?>
-                    <!-- 
+                </section>
+            </div>
+            <!-- Sidebar -->
+            <div class="col-md-3">
+                <?php dynamic_sidebar('right-sidebar'); ?>
+                <!-- 
                     <div class="right-side">
                         <h2>Title</h2>
                         <ul>
@@ -87,24 +62,14 @@
                             </li>
                         </ul>
                     </div> -->
-                </div>
             </div>
         </div>
     </div>
-    <!-- Bottom menu -->
-    <div class="bottom-menu">
-        <div class="menu">
-            <?php wp_nav_menu(array('theme_location' => 'bottommenu')); ?>
-        </div>
+</div>
+<!-- Bottom menu -->
+<div class="bottom-menu">
+    <div class="menu">
+        <?php wp_nav_menu(array('theme_location' => 'bottommenu')); ?>
     </div>
-
-    <footer>
-        <div class="container">
-            <p>@copyright | 2020 | Mainul Hasan Khan</p>
-        </div>
-    </footer>
-
-    <?php wp_footer() ; ?>
-</body>
-
-</html>
+</div>
+<?php get_footer(); ?>
