b7621850073657f3a6282a763b344543ecafa055
TechMainul <nilakashkhan2@gmail.com>
2020-11-23 06:07:18 +0600

    test

--- 404.php
+++ 404.php
@@ -1,7 +1,16 @@
 <?php get_header(); ?>
 <div class="container">
-    <h1 class="text-center">404 Error !</h1>
-
+    <div class="error-404 not-found">
+        <header class="page-header">
+            <h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+        </header><!-- .page-header -->
+        <img src="error_404.jpg" alt="error_404.jpg">
+        <div class="page-content">
+            <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?>
+            </p>
+            <?php get_search_form(); ?>
+        </div><!-- .page-content -->
+    </div><!-- .error-404 -->
 </div>
 
 <!-- Bottom menu -->
